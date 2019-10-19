3300850ee85a2c570125cfa980d94134b65d30ba
Roberto <goodknightrob@gmail.com>
2019-10-19 00:17:28 -0400

    initial commit

---
+++ cia/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using cia.Models;
+
+namespace cia.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
