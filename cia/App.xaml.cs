c54b6d058ae24f5f018fe1abd3b5a484e8497d2b
Renzo Ortiz <>
2019-10-19 18:46:44 -0400

    rest of merged changes

--- cia/App.xaml.cs
+++ cia/App.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using cia.DataStores;
 using cia.Views;
+using System.Threading.Tasks;
+
+using cia.Utils;
+using PCLStorage;
 
 namespace cia
 {
@@ -23,6 +28,18 @@ namespace cia
             await DataAccess.InitDatabase();
             var instance = DataAccessAsync.Instance;
             Console.WriteLine("DB PAth: " + instance.DatabasePath);
+
+            await TestMyMethod();
+        }
+
+        private async Task TestMyMethod()
+        {
+
+            var dummy = new AnalyzingPage(new Models.ShoppingCart(), ImageSource.FromFile("balsh"));
+
+            var path = Path.Combine(FileSystem.Current.LocalStorage.Path, "test/testreceiptImage.png");
+            var file = await EmbeddedResourceManager.CopyFileToLocation("receipt.png", path, true);
+            await dummy.GetItemsFromImage(path);
         }
 
         protected override void OnSleep()
